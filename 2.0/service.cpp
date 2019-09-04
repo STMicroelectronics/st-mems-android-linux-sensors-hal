b444afdc182138304fd48d5f9e3618a0fb833a7b
Denis Ciocca <denis.ciocca@st.com>
2019-09-04 16:13:40 -0700

    2.0: add main() function
    Signed-off-by: Denis Ciocca <denis.ciocca@st.com>
    Change-Id: I97bd3240fdb195be528d191dbfce065bae3e7c2c

---
+++ 2.0/service.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ * Copyright (C) 2019 STMicroelectronics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "android.hardware.sensors@2.0-service"
+
+#include <android/hardware/sensors/2.0/ISensors.h>
+#include <hidl/HidlTransportSupport.h>
+#include <utils/StrongPointer.h>
+#include <log/log.h>
+
+#include "Sensors.h"
+
+using android::hardware::configureRpcThreadpool;
+using android::hardware::joinRpcThreadpool;
+using android::hardware::sensors::V2_0::ISensors;
+using android::hardware::sensors::V2_0::implementation::Sensors;
+
+int main(int /* argc */, char** /* argv */)
+{
+    android::status_t err;
+
+    configureRpcThreadpool(1, true);
+
+    android::sp<ISensors> sensors = new Sensors();
+    err = sensors->registerAsService();
+    if (err != android::OK) {
+        ALOGE("Failed to register STM Sensors-HAL instance");
+        return err;
+    }
+
+    joinRpcThreadpool();
+
+    return 1;
+}
